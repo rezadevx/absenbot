82d79e2c6b7cc6e335c87f60d1ef455861f85212
rezadevs <sawe57192@gmail.com>
2024-11-26 09:48:17 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 API_ID = 23855532  # Ganti dengan API ID Anda dari https://my.telegram.org
 API_HASH = "3cc6eac0a9fbfe0b2b1da77f043cc9c9"  # Ganti dengan API Hash Anda
 BOT_TOKEN = "6942144297:AAHiwc13Y3eJyPNxt6KXqLkozjeJcb8hwN0"  # Ganti dengan token bot Anda dari BotFather
+DATABASE_FILE = "ya"  # Nama file JSON untuk menyimpan data absen
