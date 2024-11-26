cb8ff53b3c63bf58a2359609f1438680dca5a76b
rezadevs <sawe57192@gmail.com>
2024-11-26 08:58:53 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+API_ID = "your_api_id"  # Ganti dengan API ID Anda dari https://my.telegram.org
+API_HASH = "your_api_hash"  # Ganti dengan API Hash Anda
+BOT_TOKEN = "your_bot_token"  # Ganti dengan token bot Anda dari BotFather
